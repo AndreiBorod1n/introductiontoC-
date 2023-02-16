28f9781fc1cf9cee611df8bba1238b26d331a00d
Andrei Borodin <124201108+AndreiBorod1n@users.noreply.github.com>
2023-02-16 20:58:47 +0500

    Homework-1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class NewBaseType
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число: ");
+        int numberA = Console.ReadLine();
+        Console.WriteLine("Введите второе число: ");
+        int numberB = Console.ReadLine();
+        while (numberA > numberB)
+        {
+            int max = numberA;
+        }
+        int max = numberB;
+        Console.WriteLine(max);
+    }
+}
